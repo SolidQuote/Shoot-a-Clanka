80180f26107f2b1713b78597e1ddf4a525981021
SolidQuote <whatthesigmaerm091@gmail.com>
2025-08-31 03:44:37 -0400

    initial commit

---
+++ scenes/AlphaClicker/CLICKER/alpha_clicker.gd
@@ -0,0 +1,28 @@
+class_name AlphaClicker
+extends Control
+## A game where you shoot clankas
+
+## Reference to the label displaying the current amount of clankas
+@export var label : Label
+## Current amount of clankas shot
+var clankers : int = 0
+
+## Initialize the label at launch
+func _ready() -> void:
+	update_label_text()
+
+
+## Shoot 1 clanka
+func shoot_clankas() -> void:
+	clankers += 1
+	update_label_text()
+	## Update the text of the label to reflect the new change in Clankas shot
+func update_label_text() -> void:
+	label.text = "Clankers : %s" %clankers
+	
+	## Triggered when the clanker is shot
+func _on_button_pressed() -> void:
+	shoot_clankas()
+	
+	$Click.play()
+	
